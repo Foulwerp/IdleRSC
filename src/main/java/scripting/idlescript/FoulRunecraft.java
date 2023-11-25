cec204e24e46ce1cafdc4e12f5a49f0d989d2726
Foulwerp <jayrlclark@gmail.com>
2023-11-25 14:44:27 -0700

    Deposit all runes, and wait while mining

--- src/main/java/scripting/idlescript/FoulRunecraft.java
+++ src/main/java/scripting/idlescript/FoulRunecraft.java
@@ -2,16 +2,15 @@ package scripting.idlescript;
 
 import bot.Main;
 import controller.Controller;
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+import java.util.Timer;
+import java.util.concurrent.TimeUnit;
+import javax.swing.*;
 import models.entities.ItemId;
 import models.entities.MapPoint;
 import orsc.ORSCharacter;
-
-import javax.swing.*;
-import java.awt.*;
-import java.util.List;
-import java.util.Timer;
-import java.util.*;
-import java.util.concurrent.TimeUnit;
 
 public class FoulRunecraft extends IdleScript {
   public static final Controller c = Main.getController();
@@ -19,121 +18,123 @@ public class FoulRunecraft extends IdleScript {
   private String runtime, action;
   private long start;
   private int units, hourly, experience, choice = -1;
-  private final int[] bankers = new int[]{95, 268, 224, 540, 617};
-  private final int[] portals = new int[]{1228, 1229, 1230, 1231, 1232};
-  private final int[] exits = new int[]{1214, 1215, 1216, 1217, 1218, 1219, 1220, 1221, 1222};
-  private final int[] altars = new int[]{1191, 1193, 1195, 1197, 1199, 1201, 1203, 1205, 1207};
-  private final int[] ruins = new int[]{1190, 1192, 1194, 1196, 1198, 1200, 1202, 1204, 1206};
-  private final int[] talismans = new int[]{1300, 1301, 1302, 1303, 1304, 1305, 1306, 1307, 1308};
-  private final int[] doubles = new int[]{11, 14, 19, 26, 35, 46, 59, 74, 91};
-  private final int[] levels = new int[]{1, 1, 5, 9, 14, 20, 27, 35, 44};
-  private final int[] runes = new int[]{33, 35, 32, 34, 31, 36, 46, 41, 40};
-  private final String[] types = {"Air", "Mind", "Water", "Earth", "Fire", "Body", "Cosmic", "Chaos", "Nature", "Mining"};
+  private final int[] bankers = new int[] {95, 268, 224, 540, 617};
+  private final int[] portals = new int[] {1228, 1229, 1230, 1231, 1232};
+  private final int[] exits = new int[] {1214, 1215, 1216, 1217, 1218, 1219, 1220, 1221, 1222};
+  private final int[] altars = new int[] {1191, 1193, 1195, 1197, 1199, 1201, 1203, 1205, 1207};
+  private final int[] ruins = new int[] {1190, 1192, 1194, 1196, 1198, 1200, 1202, 1204, 1206};
+  private final int[] talismans = new int[] {1300, 1301, 1302, 1303, 1304, 1305, 1306, 1307, 1308};
+  private final int[] doubles = new int[] {11, 14, 19, 26, 35, 46, 59, 74, 91};
+  private final int[] levels = new int[] {1, 1, 5, 9, 14, 20, 27, 35, 44};
+  private final int[] runes = new int[] {33, 35, 32, 34, 31, 36, 46, 41, 40};
+  private final String[] types = {
+    "Air", "Mind", "Water", "Earth", "Fire", "Body", "Cosmic", "Chaos", "Nature", "Mining"
+  };
   private MapPoint[] path, reversed;
   private final MapPoint[][] paths =
-      new MapPoint[][]{
-          new MapPoint[]{
-              new MapPoint(283, 570),
-              new MapPoint(289, 579),
-              new MapPoint(298, 584),
-              new MapPoint(306, 592)
-          },
-          new MapPoint[]{
-              new MapPoint(331, 553),
-              new MapPoint(326, 544),
-              new MapPoint(324, 534),
-              new MapPoint(319, 524),
-              new MapPoint(311, 516),
-              new MapPoint(303, 508),
-              new MapPoint(303, 498),
-              new MapPoint(309, 490),
-              new MapPoint(311, 480),
-              new MapPoint(310, 470),
-              new MapPoint(309, 460),
-              new MapPoint(306, 450),
-              new MapPoint(299, 442)
-          },
-          new MapPoint[]{
-              new MapPoint(220, 636),
-              new MapPoint(210, 637),
-              new MapPoint(202, 643),
-              new MapPoint(192, 646),
-              new MapPoint(185, 654),
-              new MapPoint(176, 660),
-              new MapPoint(170, 668),
-              new MapPoint(161, 673),
-              new MapPoint(153, 681),
-              new MapPoint(150, 684),
-          },
-          new MapPoint[]{
-              new MapPoint(103, 512),
-              new MapPoint(94, 507),
-              new MapPoint(84, 507),
-              new MapPoint(76, 501),
-              new MapPoint(69, 493),
-              new MapPoint(65, 483),
-              new MapPoint(65, 473),
-              new MapPoint(63, 467),
-          },
-          new MapPoint[]{
-              new MapPoint(90, 695),
-              new MapPoint(84, 686),
-              new MapPoint(76, 679),
-              new MapPoint(72, 669),
-              new MapPoint(66, 660),
-              new MapPoint(62, 650),
-              new MapPoint(57, 641),
-              new MapPoint(52, 636)
-          },
-          new MapPoint[]{
-              new MapPoint(216, 450),
-              new MapPoint(223, 458),
-              new MapPoint(224, 468),
-              new MapPoint(224, 478),
-              new MapPoint(229, 487),
-              new MapPoint(236, 495),
-              new MapPoint(236, 505),
-              new MapPoint(243, 513),
-              new MapPoint(253, 509),
-              new MapPoint(260, 506)
-          },
-          new MapPoint[]{
-              new MapPoint(174, 3526),
-              new MapPoint(164, 3527),
-              new MapPoint(156, 3535),
-              new MapPoint(151, 3544),
-              new MapPoint(149, 3554),
-              new MapPoint(139, 3556),
-              new MapPoint(129, 3556),
-              new MapPoint(119, 3556),
-              new MapPoint(109, 3556),
-              new MapPoint(101, 3562),
-              new MapPoint(105, 3565)
-          },
-          new MapPoint[]{
-              new MapPoint(216, 450),
-              new MapPoint(220, 440),
-              new MapPoint(228, 434),
-              new MapPoint(236, 428),
-              new MapPoint(236, 418),
-              new MapPoint(235, 408),
-              new MapPoint(235, 398),
-              new MapPoint(235, 388),
-              new MapPoint(233, 378),
-              new MapPoint(233, 378)
-          },
-          new MapPoint[]{
-              new MapPoint(457, 756),
-              new MapPoint(456, 759),
-              new MapPoint(457, 769),
-              new MapPoint(448, 774),
-              new MapPoint(438, 775),
-              new MapPoint(430, 782),
-              new MapPoint(420, 787),
-              new MapPoint(411, 792),
-              new MapPoint(403, 800),
-              new MapPoint(393, 803)
-          }
+      new MapPoint[][] {
+        new MapPoint[] {
+          new MapPoint(283, 570),
+          new MapPoint(289, 579),
+          new MapPoint(298, 584),
+          new MapPoint(306, 592)
+        },
+        new MapPoint[] {
+          new MapPoint(331, 553),
+          new MapPoint(326, 544),
+          new MapPoint(324, 534),
+          new MapPoint(319, 524),
+          new MapPoint(311, 516),
+          new MapPoint(303, 508),
+          new MapPoint(303, 498),
+          new MapPoint(309, 490),
+          new MapPoint(311, 480),
+          new MapPoint(310, 470),
+          new MapPoint(309, 460),
+          new MapPoint(306, 450),
+          new MapPoint(299, 442)
+        },
+        new MapPoint[] {
+          new MapPoint(220, 636),
+          new MapPoint(210, 637),
+          new MapPoint(202, 643),
+          new MapPoint(192, 646),
+          new MapPoint(185, 654),
+          new MapPoint(176, 660),
+          new MapPoint(170, 668),
+          new MapPoint(161, 673),
+          new MapPoint(153, 681),
+          new MapPoint(150, 684),
+        },
+        new MapPoint[] {
+          new MapPoint(103, 512),
+          new MapPoint(94, 507),
+          new MapPoint(84, 507),
+          new MapPoint(76, 501),
+          new MapPoint(69, 493),
+          new MapPoint(65, 483),
+          new MapPoint(65, 473),
+          new MapPoint(63, 467),
+        },
+        new MapPoint[] {
+          new MapPoint(90, 695),
+          new MapPoint(84, 686),
+          new MapPoint(76, 679),
+          new MapPoint(72, 669),
+          new MapPoint(66, 660),
+          new MapPoint(62, 650),
+          new MapPoint(57, 641),
+          new MapPoint(52, 636)
+        },
+        new MapPoint[] {
+          new MapPoint(216, 450),
+          new MapPoint(223, 458),
+          new MapPoint(224, 468),
+          new MapPoint(224, 478),
+          new MapPoint(229, 487),
+          new MapPoint(236, 495),
+          new MapPoint(236, 505),
+          new MapPoint(243, 513),
+          new MapPoint(253, 509),
+          new MapPoint(260, 506)
+        },
+        new MapPoint[] {
+          new MapPoint(174, 3526),
+          new MapPoint(164, 3527),
+          new MapPoint(156, 3535),
+          new MapPoint(151, 3544),
+          new MapPoint(149, 3554),
+          new MapPoint(139, 3556),
+          new MapPoint(129, 3556),
+          new MapPoint(119, 3556),
+          new MapPoint(109, 3556),
+          new MapPoint(101, 3562),
+          new MapPoint(105, 3565)
+        },
+        new MapPoint[] {
+          new MapPoint(216, 450),
+          new MapPoint(220, 440),
+          new MapPoint(228, 434),
+          new MapPoint(236, 428),
+          new MapPoint(236, 418),
+          new MapPoint(235, 408),
+          new MapPoint(235, 398),
+          new MapPoint(235, 388),
+          new MapPoint(233, 378),
+          new MapPoint(233, 378)
+        },
+        new MapPoint[] {
+          new MapPoint(457, 756),
+          new MapPoint(456, 759),
+          new MapPoint(457, 769),
+          new MapPoint(448, 774),
+          new MapPoint(438, 775),
+          new MapPoint(430, 782),
+          new MapPoint(420, 787),
+          new MapPoint(411, 792),
+          new MapPoint(403, 800),
+          new MapPoint(393, 803)
+        }
       };
   private final Timer timer = new Timer();
   private final TimerTask task =
@@ -249,7 +250,8 @@ public class FoulRunecraft extends IdleScript {
           while (c.isCurrentlyWalking()) c.sleep(ticks(1));
           return ticks(2);
         }
-        if (c.getInventoryItemCount(10) < 5 * (30 - c.getInventoryItemCount()) || c.getInventoryItemCount(1299) < 30 - c.getInventoryItemCount()) {
+        if (c.getInventoryItemCount(10) < 5 * (30 - c.getInventoryItemCount())
+            || c.getInventoryItemCount(1299) < 30 - c.getInventoryItemCount()) {
           c.log("Out rune stones or coins!");
           c.log("Crafted " + units + " runes at " + hourly + " per hour.");
           c.stop();
@@ -277,7 +279,7 @@ public class FoulRunecraft extends IdleScript {
           c.stop();
           return ticks(1);
         }
-        if (choice == 7 && c.isItemInInventory(runes[choice])) {
+        if (c.isItemInInventory(runes[choice])) {
           c.depositItem(runes[choice], c.getInventoryItemCount(runes[choice]));
           return ticks(1);
         }
@@ -291,6 +293,7 @@ public class FoulRunecraft extends IdleScript {
 
   private int miningLoop() {
     if (c.getInventoryItemCount() < 30) {
+      if (c.getPlayer().bubbleTimeout > 0) return ticks(1);
       int[] essence = c.getNearestObjectById(1227);
       if (essence != null) {
         c.atObject(essence[0], essence[1]);
